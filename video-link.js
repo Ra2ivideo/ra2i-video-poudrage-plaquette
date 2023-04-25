be0f78df2c4b78b71967b097d9a567fbdb99c060
Ra2ivideo <131665263+Ra2ivideo@users.noreply.github.com>
2023-04-25 17:00:47 +0200

    Create video-link.js

---
+++ video-link.js
@@ -0,0 +1,7 @@
+(function() {
+  const videoLink = "https://www.canva.com/design/DAFhHsHJ8-o/watch?embed";
+  
+  window.addEventListener('DOMContentLoaded', function() {
+    const videoIframe = document.getElementById('videoIframe');
+    if (videoIframe) {
+      videoIframe.src
