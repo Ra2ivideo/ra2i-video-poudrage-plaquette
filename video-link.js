3324204d9cc58fc5ec270fc1bc956dc7dee703d5
Ra2ivideo <131665263+Ra2ivideo@users.noreply.github.com>
2023-04-25 17:06:07 +0200

    Delete video-link.js

--- video-link.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  const videoLink = "https://www.canva.com/design/DAFhHsHJ8-o/watch?embed";
-  
-  window.addEventListener('DOMContentLoaded', function() {
-    const videoIframe = document.getElementById('videoIframe');
-    if (videoIframe) {
-      videoIframe.src
